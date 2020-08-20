9eed2ef6568bb3616c6b1320795355a7bea2071e
nikhil <nikhil.guptha23@gmail.com>
2020-08-20 21:30:58 +0530

    create jenkins job for helloworld using maven

---
+++ src/main/java/srinfo/com/App.java
@@ -0,0 +1,13 @@
+package srinfo.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Devops Engineers" );
+    }
+}
---
+++ src/test/java/srinfo/com/AppTest.java
@@ -0,0 +1,20 @@
+package srinfo.com;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
