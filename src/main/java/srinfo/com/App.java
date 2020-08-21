4a297ca26301108b0c3a27532000f337cbde783d
nikhil <nikhil.guptha23@gmail.com>
2020-08-21 21:19:34 +0530

    done code changes need to be deployed immediately

--- src/main/java/srinfo/com/App.java
+++ src/main/java/srinfo/com/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Devops Engineers" );
+        System.out.println( "Hello Devops Engineers, making changes in code" );
     }
 }
